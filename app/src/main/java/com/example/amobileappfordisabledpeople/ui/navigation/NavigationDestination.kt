008f74ac54ae18f7410fe203574a471b96bfda2d
theobeo2004 <thenhuet2004@gmail.com>
2024-10-25 14:20:15 +0700

    feat: implement destinations corresponding to screens

---
+++ app/src/main/java/com/example/amobileappfordisabledpeople/ui/navigation/NavigationDestination.kt
@@ -0,0 +1,24 @@
+package com.example.amobileappfordisabledpeople.ui.navigation
+
+import com.example.amobileappfordisabledpeople.R
+
+interface NavigationDestination {
+    val route: String
+    val titleRes: Int
+}
+
+object DetectionDestination : NavigationDestination {
+    override val route = "detection"
+    override val titleRes = R.string.detection
+}
+
+object DangerWarningDestination : NavigationDestination {
+    override val route = "danger_warning"
+    override val titleRes = R.string.danger_warning
+}
+
+object ExploreDestination: NavigationDestination {
+    override val route = "explore"
+    override val titleRes = R.string.explore
+
+}
