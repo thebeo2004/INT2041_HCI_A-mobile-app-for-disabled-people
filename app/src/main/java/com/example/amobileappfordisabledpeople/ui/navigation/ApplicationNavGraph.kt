85ccddb00b732057fec9602a90107163ce6b5782
theobeo2004 <thenhuet2004@gmail.com>
2024-10-25 14:43:39 +0700

    branch: navigation_bar - feat: implement destinations corresponding to screens

---
+++ app/src/main/java/com/example/amobileappfordisabledpeople/ui/navigation/ApplicationNavGraph.kt
@@ -0,0 +1,30 @@
+package com.example.amobileappfordisabledpeople.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun ApplicationNavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DetectionDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = DetectionDestination.route) {
+//            DetectionScreen()
+        }
+        composable(route = DangerWarningDestination.route) {
+//            DangerWarningScreen()
+        }
+        composable(route = ExploreDestination.route) {
+//            ExploreScreen()
+
+        }
+    }
+}
