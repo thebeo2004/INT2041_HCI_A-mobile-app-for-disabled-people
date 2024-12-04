166d1ef1958fecb40300ebdbb176d4371112ef9e
theobeo2004 <thenhuet2004@gmail.com>
2024-12-04 21:38:06 +0700

    mood_tracking branch - feat: adding new string resources in strings.xml, and implement new route objects corresponding to new features

--- app/src/main/java/com/example/amobileappfordisabledpeople/ui/navigation/NavigationDestination.kt
+++ app/src/main/java/com/example/amobileappfordisabledpeople/ui/navigation/NavigationDestination.kt
@@ -21,3 +21,13 @@ object ExploreDestination: NavigationDestination {
     override val route = "explore"
     override val titleRes = R.string.explore
 }
+
+object MoodTrackingDestination: NavigationDestination {
+    override val route = "tracking"
+    override val titleRes = R.string.mood_tracking
+}
+
+object FaceRecognition: NavigationDestination {
+    override val route = "face_recognition"
+    override val titleRes = R.string.face_recognition
+}
